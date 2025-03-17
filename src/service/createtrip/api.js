996a2886cb5eca550a0b03cacd9206d908ca565d
Feras Yahya <103768739+ferasyahya@users.noreply.github.com>
2025-03-17 01:33:55 -0400

    Changed URL to local host 8080

--- src/service/createtrip/api.js
+++ src/service/createtrip/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const BASE_URL = "http://localhost:5000/create-trip";
+const BASE_URL = "http://localhost:8080/api/create-trip";
 
 // Set the base URL for your API
 const axiosInstance = axios.create({
