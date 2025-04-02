ef494ec86939cb7d854277783e40725003aec40d
Feras Yahya <103768739+ferasyahya@users.noreply.github.com>
2025-04-01 22:49:59 -0400

    Added stats grid to the home page

--- src/home/StatsGrid.jsx
+++ src/home/StatsGrid.jsx
@@ -1,21 +1,45 @@
 import { cn } from '@/lib/utils'
 import React from 'react'
+import { FaPlane, FaGlobe, FaUsers } from 'react-icons/fa';
+import StatCard from './StatCard'
 
 const StatsGrid = ({
-    children,
-    gap = 'gap-10',
-    cols = { base: 1, md: 3 },
+    gap = 'gap-15',
     className
 }) => {
     return (
         <div className={cn(
-            'grid mx-auto',
-            'grid-cols-${cols.base}',
-            'md:grid-cols-${cols.md}',
+            'flex flex-col md:flex-row',
+            'justify-center',
+            'p-10 w-full',
+            'bg-gray-200',
             gap,
             className
         )}>
-            {children}
+            <StatCard
+                image={FaPlane}
+                firstLabel="23,000+"
+                secondLabel="Travel Experience"
+                className="w-70"
+            />
+            <StatCard
+                image={FaGlobe}
+                firstLabel="40+"
+                secondLabel="Countries"
+                className="w-70"
+            />
+            <StatCard
+                image={FaUsers}
+                firstLabel="10,000+"
+                secondLabel="Happy Travellers"
+                className="w-70"
+            />
+            <StatCard
+                image={FaUsers}
+                firstLabel="10,000+"
+                secondLabel="Happy Travellers"
+                className="w-70"
+            />
         </div>
     )
 }
