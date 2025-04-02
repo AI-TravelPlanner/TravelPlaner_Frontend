b7a67f9fc1616704ef6d28eec06c0f143c6ee187
Feras Yahya <103768739+ferasyahya@users.noreply.github.com>
2025-04-02 13:24:33 -0400

    Added customer grid component to home page

--- src/home/HomePage.jsx
+++ src/home/HomePage.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import ImageCarousel from "./ImageCarcousel";
+import CustomerGrid from "./CustomerGrid";
 
 const HomePage = () => {
   return (
     <div>
       {/* Image Carousel component */}
       <ImageCarousel />
+      <CustomerGrid />
     </div>
   );
 };
