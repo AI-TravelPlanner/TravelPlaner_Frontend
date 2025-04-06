13d3cfe17a09c71cf803b1c937aa1fccb609c1c6
Feras Yahya <103768739+ferasyahya@users.noreply.github.com>
2025-04-06 00:28:27 -0400

    Added new components to be implemented

---
+++ src/createtrip/tripComponents/DayCard.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const DayCard = () => {
+    return (
+        <div className='grid grid-cols-2 w-full'>
+            <div>Column 1</div>
+            <div>Column 2</div>
+        </div>
+    )
+}
+
+export default DayCard
---
+++ src/createtrip/tripComponents/InfoDisplayCard.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const InfoDisplayCard = () => {
+    return (
+        // This component will be used to display the information of the trip
+        <div>InfoDisplayCard</div>
+    )
+}
+
+// This component will be used to display the information of the trip
+export default InfoDisplayCard
