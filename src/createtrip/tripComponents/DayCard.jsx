d69035763ab76e3d64b819980ed8bb3cc3a0d215
Feras Yahya <103768739+ferasyahya@users.noreply.github.com>
2025-04-07 23:29:52 -0400

    Added hotel and weather cards

--- src/createtrip/tripComponents/DayCard.jsx
+++ src/createtrip/tripComponents/DayCard.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 import ActivityCard from "./ActivityCard";
+import HotelCard from "./HotelCard";
+import WeatherCard from "./WeatherCard";
 
 const DayCard = () => {
   return (
@@ -12,11 +14,30 @@ const DayCard = () => {
         <div className="col-span-4 p-1">
           <div className="grid grid-rows-[80%_20%] gap-2 h-full">
             <div className="bg-gray-100 text-center border border-black rounded">
-              Row 1 (80%)
+              {/* Row 1 (80%) */}
+              <HotelCard
+                hotelName="Sheraton"
+                hotelAddress="Hotel Address"
+                hotelRating="Hotel Rating"
+                hotelPrice="$100"
+                hotelImage="/src/assets/hotel-img/EmiratesHotel.webp"
+                hotelCheckInDate={new Date().toLocaleDateString()}
+                hotelCheckOutDate={new Date().toLocaleDateString()}
+                hotelRoomType="Deluxe Room"
+                hotelAmenities="Free Wi-Fi, Pool, Gym"
+                hotelBookingLink="www.example.com"
+                hotelContactNumber="123-456-7890"
+                hotelDescription="Hotel Description"
+                hotelNearbyAttractions="Nearby Attractions"
+              />
             </div>
 
             <div className="bg-gray-300 border border-black rounded">
-              Row 2 (20%)
+              {/* Row 2 (20%) */}
+              <WeatherCard
+                temperature={25}
+                weatherDescription="Rainy"
+              />
             </div>
           </div>
         </div>
