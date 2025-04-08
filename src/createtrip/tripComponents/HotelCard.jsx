3ebfde00761c230cd2f417d5d6bca5174919b209
Feras Yahya <103768739+ferasyahya@users.noreply.github.com>
2025-04-08 04:27:03 -0400

    added basic hotel image as a placeholder

--- src/createtrip/tripComponents/HotelCard.jsx
+++ src/createtrip/tripComponents/HotelCard.jsx
@@ -20,7 +20,7 @@ const HotelCard = ({ hotelDetails }) => {
   return (
     <div
       className={cn(
-        "cursor-pointer w-full h-full min-h-0 min-w-0 dark:border-gray-700 bg-white dark:bg-gray-800"
+        "cursor-pointer w-full h-full min-h-0 min-w-0 dark:border-gray-700 dark:bg-gray-800"
       )}
       onClick={handleFlip}
     >
@@ -34,21 +34,24 @@ const HotelCard = ({ hotelDetails }) => {
         }}
       >
         <div
-          className="absolute inset-0 w-full h-full rounded-lg shadow-xl border px-3 grid grid-row-3"
+          className="absolute inset-0 grid grid-row-4 "
           style={{
             backfaceVisibility: "hidden",
           }}
         >
-          <img
-            src="https://via.placeholder.com/150"
-            alt="Activity"
-            className="w-full h-auto object-cover"
-          />
-          <h2 className="font-bold">{hotelDetails.hotelName}</h2>
+          <div className="px-3">
+            <img
+              // src="https://via.placeholder.com/150"
+              src="/src/assets/hotel-img/Sheraton.jpg"
+              alt="Activity"
+              className="w-full h-auto object-cover"
+            />
+          </div>
+          <h2 className="text-[75%] font-bold">{hotelDetails.hotelName}</h2>
           <p className="text-xs text-gray-600">
             {hotelDetails.pricePerNight}/Night
           </p>
-          <p className="text-xs text-gray-600">{hotelDetails.location}</p>
+          {/* <p className="text-xs text-gray-600">{hotelDetails.location}</p> */}
         </div>
       </motion.div>
     </div>
